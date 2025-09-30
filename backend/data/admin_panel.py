cd021ee2639109ee6159fd2861547f06a1f574e9
Aleksandr Bynkov <bixber.commercial@yandex.ru>
2025-09-30 04:07:45 +0300

    Rewrite the existing models and add the new models 'Question' and 'Content'

--- backend/data/admin_panel.py
+++ backend/data/admin_panel.py
@@ -1,10 +1,9 @@
 from contextlib import asynccontextmanager
 from fastapi import FastAPI
 from sqladmin import Admin, ModelView
-from sqladmin.filters import ForeignKeyFilter
-from passlib.context import CryptContext
+from sqladmin.filters import BooleanFilter, ForeignKeyFilter
 
-from .models import User, Button, Category, Role
+from .models import User, Category, Content, Question
 from .db import engine, create_db_and_tables
 
 
@@ -19,112 +18,135 @@ app = FastAPI(
     lifespan=lifespan,
 )
 
-admin = Admin(app, engine, title="Управление IHearYou Ботом")
+admin = Admin(app, engine, title="IHearYou Admin")
 
 
 class UserAdmin(ModelView, model=User):
     name = 'Пользователь'
     name_plural = 'Пользователи'
     column_list = (
-        User.id,
-        User.username,
-        'full_name',
-        User.birth_date,
-        User.role_id,
+        User.telegram_id,
+        User.is_blocked,
+        User.registered_at,
     )
     column_labels = {
-        User.id: 'Идентификатор',
-        User.name: 'Имя',
+        User.telegram_id: 'Идентификатор (Telegram ID)',
         User.username: 'Никнейм',
-        User.surname: 'Фамилия',
-        User.patronymic: 'Отчество',
         User.password: 'Пароль',
-        User.birth_date: 'Дата рождения',
-        User.role: 'Роль',
-        User.role_id: 'Роль',
-        'full_name': 'Полное имя',
+        User.is_blocked: 'Статус блокировки',
+        User.is_admin: 'Статус администратора',
+        User.registered_at: 'Дата и время регистрации',
     }
-    column_searchable_list = [User.username, User.name, User.surname]
-    column_sortable_list = [User.id, User.username, User.birth_date]
-    form_edit_rules = [
-        'name',
-        'surname',
-        'patronymic',
-        'birth_date',
-        'role',
-    ]
-    icon = 'fa-solid fa-user'
-
-    async def on_model_change(self, data, model, is_created, request) -> None:
-        if is_created:
-            pwd_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
-            data['password'] = pwd_context.hash(data['password'])
-
-
-class ButtonAdmin(ModelView, model=Button):
-    name = 'Кнопка'
-    name_plural = 'Кнопки'
-    column_exclude_list = [Button.category]
+    form_rules = ['is_blocked']  # админ может только блокировать пользователей
+    column_searchable_list = [User.telegram_id]
+    column_sortable_list = [User.registered_at, User.is_blocked]
     column_filters = [
-        ForeignKeyFilter(
-            Button.category_id,
-            Category.title,
-            title='По категории',
-        ),
+        BooleanFilter(User.is_blocked, 'По блокировке'),
+    ]
+    can_create = True  # после деплоя на сервер, переключить на значение False
+    identity = 'user'
+
+
+class ContentAdmin(ModelView, model=Content):
+    name = 'Контент'
+    name_plural = 'Контенты'
+    column_exclude_list = [
+        Content.image_path,
+        Content.url_link,
+        Content.category_id,
+        Content.category,
+        Content.text,
     ]
     column_labels = {
-        Button.id: 'Идентификатор',
-        Button.title: 'Название',
-        Button.description: 'Описание',
-        Button.category: 'Категория',
-        Button.category_id: 'Категория',
-        Button.content: 'Контент',
+        Content.id: 'Идентификатор',
+        Content.title: 'Текст кнопки',
+        Content.description: 'Краткое описание',
+        Content.text: 'Тектовое содержимое',
+        Content.image_path: 'Изображение',
+        Content.url_link: 'Ссылка к основному тексту',
+        Content.is_active: 'Статус активности',
+        Content.category: 'Категория',
+        Content.category_id: 'Категория',
+        Content.created_at: 'Дата и время создания',
     }
-    column_searchable_list = [Button.title, Button.category]
-    column_sortable_list = [Button.title, Button.category]
+    column_searchable_list = [Content.title, Content.text, Content.description]
+    column_sortable_list = [
+        Content.title,
+        Content.created_at,
+        Content.is_active,
+    ]
+    form_rules = [
+        'title',
+        'description',
+        'text',
+        'image_path',
+        'url_link',
+        'is_active',
+        'category',
+    ]
+    column_filters = [
+        BooleanFilter(Content.is_active, 'По активности'),
+        ForeignKeyFilter(
+            Content.category_id,
+            Category.title,
+            title='По категориям'
+        ),
+    ]
 
 
 class CategoryAdmin(ModelView, model=Category):
     name = 'Категория'
     name_plural = 'Категории'
-    column_exclude_list = [Category.for_user_role, Category.buttons]
+    column_exclude_list = [Category.contents]
     column_labels = {
         Category.id: 'Идентификатор',
-        Category.slug: 'Короткое название',
-        Category.title: 'Название',
-        Category.description: 'Описание',
-        Category.for_user_role: 'Предназначена для',
-        Category.for_user_role_id: 'Предназначена для',
-        Category.buttons: 'Кнопки',
+        Category.title: 'Название кнопки',
+        Category.description: 'Краткое описание',
+        Category.is_active: 'Статус активности',
+        Category.created_at: 'Дата и время создания',
     }
-    column_searchable_list = [Category.slug, Category.title]
+    form_rules = [
+        'title',
+        'description',
+        'is_active',
+    ]
+    column_searchable_list = [Category.title, Category.description]
     column_sortable_list = [
-        Category.slug,
         Category.title,
-        Category.for_user_role,
+        Category.is_active,
+        Category.created_at,
+    ]
+    column_filters = [
+        BooleanFilter(Category.is_active, 'По активности'),
     ]
 
 
-class RoleAdmin(ModelView, model=Role):
-    name = 'Роль'
-    name_plural = 'Роли'
-    column_exclude_list = [Role.users, Role.categories]
+class QuestionAdmin(ModelView, model=Question):
+    name = 'Запрос от пользователя'
+    name_plural = 'Запросы от пользователей'
+    column_exclude_list = [Question.user_id]
     column_labels = {
-        Role.id: 'Идентификатор',
-        Role.slug: 'Короткое название',
-        Role.title: 'Название',
-        Role.description: 'Описание',
-        Role.users: 'Пользователи',
-        Role.categories: 'Категории',
+        Question.id: 'Идентификатор',
+        Question.text: 'Текст',
+        Question.user_id: 'Пользователь',
+        Question.created_at: 'Дата и время создания запроса',
+        Question.user: 'Пользователь',
     }
-    column_searchable_list = [Role.slug, Role.title]
-    column_sortable_list = [Role.title, Role.slug, Role.categories]
+    form_rules = [
+        'text',
+        'user',
+    ]
+    column_searchable_list = [Question.text, Question.user_id]
+    column_sortable_list = [Question.created_at]
+    can_create = True  # после деплоя на сервер, переключить на значение False
+    can_edit = False
+    can_delete = True
 
 
 admin.add_view(UserAdmin)
-admin.add_view(ButtonAdmin)
 admin.add_view(CategoryAdmin)
-admin.add_view(RoleAdmin)
+admin.add_view(ContentAdmin)
+admin.add_view(QuestionAdmin)
 
 if __name__ == "__main__":
     import uvicorn
--- backend/data/models.py
+++ backend/data/models.py
@@ -1,80 +1,79 @@
 from typing import Optional
-from datetime import date
+from datetime import datetime
 
-from sqlmodel import SQLModel, Relationship, Field
-
+from sqlmodel import SQLModel, Field, Relationship
+from sqlalchemy import Column, BigInteger, DateTime, func
 from passlib.context import CryptContext
+
+from .mixins import BaseInfoMixin
+
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 
-class Role(SQLModel, table=True):
-    __tablename__ = 'users_roles'
+class Category(BaseInfoMixin, SQLModel, table=True):
+    __tablename__ = 'categories'
 
-    id: Optional[int] = Field(default=None, primary_key=True,)
-    slug: str = Field(unique=True,)
-    title: str = Field(unique=True)
-    description: Optional[str]
-
-    users: list['User'] = Relationship(
-        back_populates='role',
-        sa_relationship_kwargs={'lazy': 'selectin'},
+    created_at: datetime = Field(
+        sa_column=Column(DateTime(timezone=True), server_default=func.now()),
     )
-    categories: list['Category'] = Relationship(
-        back_populates='for_user_role',
-        sa_relationship_kwargs={'lazy': 'selectin'},
-    )
+    is_active: bool
 
-
-class Category(SQLModel, table=True):
-    __tablename__ = 'buttons_categories'
-
-    id: Optional[int] = Field(default=None, primary_key=True)
-    slug: str = Field(unique=True)
-    title: str = Field(unique=True)
-    description: Optional[str]
-
-    for_user_role_id: int = Field(foreign_key=f'{Role.__tablename__}.id')
-    for_user_role: Role = Relationship(back_populates='categories')
-
-    buttons: list['Button'] = Relationship(
+    contents: list['Content'] = Relationship(
         back_populates='category',
         sa_relationship_kwargs={'lazy': 'selectin'},
     )
 
 
-class Button(SQLModel, table=True):
-    __tablename__ = 'buttons'
+class Content(BaseInfoMixin, SQLModel, table=True):
+    __tablename__ = 'contents'
 
-    id: Optional[int] = Field(
-        default=None,
-        primary_key=True,
+    text: Optional[str]
+    image_path: Optional[str]  # реализовать хранение файла изображения
+    url_link: Optional[str]
+    is_active: bool
+    created_at: datetime = Field(
+        sa_column=Column(DateTime(timezone=True), server_default=func.now()),
     )
-    title: str = Field(unique=True, index=True)
-    description: Optional[str]
-    content: Optional[str]
 
     category_id: int = Field(foreign_key=f'{Category.__tablename__}.id')
-    category: Category = Relationship(back_populates='buttons')
+    category: Category = Relationship(
+        back_populates='contents',
+        sa_relationship_kwargs={'lazy': 'selectin'},
+    )
 
 
 class User(SQLModel, table=True):
     __tablename__ = 'users'
 
-    id: Optional[int] = Field(default=None, primary_key=True,)
-    username: str = Field(unique=True, index=True)
-    password: str
-    name: Optional[str] = Field(index=True)
-    surname: Optional[str] = Field(index=True)
-    patronymic: Optional[str] = Field(index=True)
-    birth_date: Optional[date]
+    telegram_id: int = Field(
+        sa_column=Column(BigInteger, primary_key=True, autoincrement=False),
+    )
+    username: Optional[str] = Field(unique=True, index=True)
+    password: Optional[str]
+    is_blocked: bool
+    is_admin: bool
+    registered_at: datetime = Field(
+        sa_column=Column(DateTime(timezone=True), server_default=func.now())
+    )
 
-    role_id: int = Field(foreign_key=f'{Role.__tablename__}.id')
-    role: Role = Relationship(
-        back_populates='users',
+    questions: list['Question'] = Relationship(
+        back_populates='user',
         sa_relationship_kwargs={'lazy': 'selectin'},
     )
 
-    @property
-    def full_name(self) -> str:
-        return f"{self.name} {self.surname}"
+
+class Question(SQLModel, table=True):
+    __tablename__ = 'questions'
+
+    id: Optional[int] = Field(default=None, primary_key=True)
+    text: str
+    created_at: datetime = Field(
+        sa_column=Column(DateTime(timezone=True), server_default=func.now()),
+    )
+
+    user_id: int = Field(foreign_key=f'{User.__tablename__}.telegram_id')
+    user: User = Relationship(
+        back_populates='questions',
+        sa_relationship_kwargs={'lazy': 'selectin'},
+    )
