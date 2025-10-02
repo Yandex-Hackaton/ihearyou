57430fb67ae07cfe8ffebb8448542f31aa39c9df
Kristina Alabova <kristina.alabova@mail.ru>
2025-10-02 10:22:15 +0300

    fix ADMINS, ADMIN_QUESTION_URL

--- backend/bot/handlers/callbacks.py
+++ backend/bot/handlers/callbacks.py
@@ -31,8 +31,17 @@ from utils.logger import logger
 
 callback_router = Router()
 
-ADMINS = [int(admin_id) for admin_id in os.getenv("ADMINS", "").split(',')]
-# ADMINS = [int(admin_id) for admin_id in os.getenv("1046168795", "1046168795").split(',')]
+admins_raw_str = os.getenv("ADMINS", "")
+ADMINS = []
+if admins_raw_str:
+    for admin_id_str in admins_raw_str.split(','):
+        try:
+            ADMINS.append(int(admin_id_str.strip()))
+        except ValueError:
+            logger.warning(
+                f"Некорректный ID администратора в .env: '{admin_id_str}'."
+                )
+
 ADMIN_QUESTION_URL = "https://www.ihearyou.ru//admin/question/details/"
 
 
